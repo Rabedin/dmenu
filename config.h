a0b06036f92c154f4518d5a3c69255888aaa5a41
Rabedin <93693987+Rabedin@users.noreply.github.com>
2021-11-10 21:42:49 -0500

    Initial commit
    Uploading files from my current desktop version of dmenu.

---
+++ config.h
@@ -0,0 +1,32 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;				/* -b option; if 0, dmenu appears at bottom	*/
+static int centered = 0;			/* -c option; centers dmenu on screen 		*/
+static int min_width = 500;			/* minimum width when centered			*/
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"iosevka:size=16"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+				        /*    fg         bg   */
+		[SchemeNorm]          = { "#b4b5bd", "#282a36" },
+		[SchemeSel]           = { "#f8f8f2", "#44475a" },
+		[SchemeSelHighlight]  = { "#ffb86c", "#44475a" },
+		[SchemeNormHighlight] = { "#ff79c6", "#282a36" },
+		[SchemeOut]           = { "#000000", "#8be9fd" },
+		[SchemeOutHighlight]  = { "#ffb86c", "#8be9fd" },
+};
+/* -l and -g options; controls number of lines and columns in grid if > 0 */
+static unsigned int lines	= 0;
+static unsigned int columns	= 0;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
+
+/* Size of the window border */
+static const unsigned int border_width =2;
